9ef163fa86c7dfff0e3920a0ead0f5047b13e471
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-04-03 15:06:52 -0400

    Create main.cpp

--- Week9_notes/main.cpp
+++ Week9_notes/main.cpp
@@ -10,9 +10,8 @@ int main()
     r1.printName(); //using rectangle object to print the name (rectangle)
     cout << r1.add() << endl; // 9. Because the length is 4 and the width is 5. 
     cout << r1.add(7) << endl; // 16 because 4 + 5 + 7 = 16.
-    r1.add("bowling",11);
-    cout << endl << r2.add(); //using the alt. Rectangle constructor 
+    r1.add("bowling",11); //inserting "bowling" and 11 as parameters for the .add(string, int) function.
+    cout << endl << r2.add(); //using the alt. Rectangle constructor. adds 50 + 3 = 53
   
     return 0;
 }
-
