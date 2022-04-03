5348e84ca5b156cd9b2fe7579197a555eeb09d40
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-04-03 15:08:24 -0400

    Update Shape.cpp

--- Week9_notes/Shape.cpp
+++ Week9_notes/Shape.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //Shape functions
 
-//constructor (Must set up initial values w/ constructors)
+//constructor (Must set up initial values w/ constructors using the variables under protected/private)
 Shape::Shape()
 {
     name = "shape"; //setting initial values
@@ -46,3 +46,14 @@ void Rectangle::add(string b, int c)//overload
 {
     cout << b << " is not a number but " << c << " is";
 }
+
+
+/*
+
+
+- which constructor you choose depends on what you do with the Rectangle object. 
+
+- r1 would go to the first constructor w/o parameters.
+- r2 would go to the constructor with parameters. that's why they print out different results
+
+*/
